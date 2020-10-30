eca82c3f40e00735c4e6ba42392c4c1314e5054a
Julia Ballo <ballo.julia@gmail.com>
2020-10-30 00:35:33 -0700

    First Commit

---
+++ jsClassList.js
@@ -0,0 +1,5 @@
+// WRITE YOUR CODE IN HERE:
+const listItems = document.getElementsByTagName("LI");
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].classList.toggle("highlight");
+} 
